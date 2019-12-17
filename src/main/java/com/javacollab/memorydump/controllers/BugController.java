0da43211f49b2a04ca08b4c21be3dd4fd05cfd0f
Lia McClane <lia_mcclane@msn.com>
2019-12-17 09:00:16 -0800

    more jps files and static folder started

--- src/main/java/com/javacollab/memorydump/controllers/BugController.java
+++ src/main/java/com/javacollab/memorydump/controllers/BugController.java
@@ -1,5 +1,30 @@
 package com.javacollab.memorydump.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bug")
 public class BugController {
+
+    private UserService userService;
+
+
+
+
+    public BugController(UserService userService){
+        this.userService = userService;
+    }
+
+    
+    @RequestMapping("/{bug_id}")
+    public String showBug(
+        Model model){
+
+
+
+            return "display.jsp";
+    }
+
 
 }

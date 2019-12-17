449aea881cb426e3c77c8c9e1b600aeac998562b
Toan Tran <songtoan0201@gmail.com>
2019-12-17 11:32:04 -0800

    minor changes Toan

--- src/main/java/com/javacollab/memorydump/repositories/BugRepo.java
+++ src/main/java/com/javacollab/memorydump/repositories/BugRepo.java
@@ -1,5 +1,13 @@
 package com.javacollab.memorydump.repositories;
 
-public interface BugRepo {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javacollab.memorydump.models.Bug;
+
+
+
+@Repository
+public interface BugRepo extends CrudRepository<Bug, Long>{
 
 }

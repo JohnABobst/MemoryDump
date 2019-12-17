6e6bbfdaca900a32f4d7d8beb824aea547cb4485
liamcclane <lia_mcclane@msn.com>
2019-12-17 12:58:55 -0800

    choose a bug service file

--- src/main/java/com/javacollab/memorydump/services/BugService.java
+++ src/main/java/com/javacollab/memorydump/services/BugService.java
@@ -1,19 +1,5 @@
 package com.javacollab.memorydump.services;
 
-<<<<<<< HEAD
-import com.javacollab.memorydump.repositories.BugRepo;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BugService {
-
-    private final BugRepo bugRepo;
-
-    public BugService(BugRepo bugRepo){
-        this.bugRepo = bugRepo;
-    }
-=======
 import java.util.Optional;
 
 import com.javacollab.memorydump.models.Bug;
@@ -24,17 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class BugService {
     private final BugRepo bugRepository;
-        public BugService(BugRepo bugRepository) {
-            this.bugRepository = bugRepository;
+  
+    public BugService(BugRepo bugRepository) {
+        this.bugRepository = bugRepository;
+    }
+  
+  
+    public Bug findBugById(Long id) {
+        Optional<Bug> u = bugRepository.findById(id);
+        
+        if(u.isPresent()) {
+            return u.get();
+        } else {
+            return null;
         }
-        public Bug findBugById(Long id) {
-            Optional<Bug> u = bugRepository.findById(id);
-            
-            if(u.isPresent()) {
-                return u.get();
-            } else {
-                return null;
-            }
-        }
->>>>>>> d5c805436891a460581ebd997fb34702174ab651
+    }
 }

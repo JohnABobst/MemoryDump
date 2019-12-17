7b42ed216a2d2c79d57b29a14093562c3146f831
liamcclane <lia_mcclane@msn.com>
2019-12-17 12:52:58 -0800

    worked on controllers and routing

--- src/main/java/com/javacollab/memorydump/controllers/AllBugRouts.java
+++ src/main/java/com/javacollab/memorydump/controllers/AllBugRouts.java
@@ -1,17 +1,10 @@
 package com.javacollab.memorydump.controllers;
 
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
+import com.javacollab.memorydump.repositories.BookmarkRepo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javacollab.memorydump.models.Bug;
 import com.javacollab.memorydump.models.User;
@@ -26,9 +19,10 @@ import com.javacollab.memorydump.services.UserService;
 import com.javacollab.memorydump.validators.UserValidator;
 
 @Controller
-public class BugController {
-	
-	private final BookmarkRepo bookmarkRepository;
+@RequestMapping("/bug")
+public class AllBugRouts {
+
+    private final BookmarkRepo bookmarkRepository;
 	private final BugRepo bugRepository;
 	private final CommentRepo commentRepository;
 	private final StepRepo stepRepository;
@@ -44,7 +38,7 @@ public class BugController {
 
 	private final UserValidator userValidator;
 
-	public BugController(
+	public AllBugRouts(
 			BookmarkRepo bookmarkRepository,
 			BugRepo bugRepository,
 			CommentRepo commentRepository,
@@ -76,59 +70,6 @@ public class BugController {
 		this.userService = userService;
 		this.userValidator = userValidator;
 	
-	}
-
-	@GetMapping("/")
-	public String regAndLogin(Model model) {
-		User user_r = new User();
-		
-		model.addAttribute("user_r", user_r);
-		
-		return "logReg.jsp";
-	}
-	
-	@PostMapping("/registration")
-	public String register(@Valid @ModelAttribute("user_r") User user, BindingResult result, HttpSession session) {
-		
-		userValidator.validate(user, result);
-        if(result.hasErrors()) {
-            return "logReg.jsp";
-        }
-        User u = userService.registerUser(user);
-        
-        session.setAttribute("userId", u.getId());
-        session.setAttribute("userName", u.getName());
-        
-        return "redirect:/dashboard";
-	}
-	
-	@PostMapping("/login")
-	public String login(@ModelAttribute("user_r") User user, @RequestParam("email_l") String email, @RequestParam("password_l") String password, Model model, HttpSession session) {
-		
-		boolean isAuthenticated = userService.authenticateUser(email, password);
-		if(isAuthenticated) {
-			User u = userService.findByEmail(email);
-			session.setAttribute("userId", u.getId());
-			session.setAttribute("userName", u.getName());
-			return "redirect:/dashboard";
-		} else {
-			model.addAttribute("error", "Invalid Credentials. Please try again");
-			return "logReg.jsp";
-		}
-	}
-	
-	@GetMapping("/dashboard")
-	public String dashboard(
-        Model model,
-        HttpSession session) {
-
-       // User u = userService.findUserById((Long) session.getAttribute("userId"));
-        // place holder for now untill we get just the users specific list of bugs
-       List<Bug> bugs = bugRepository.findAll();
+    }
     
-        // model.addAttribute("user", u);
-        // model.addAttribute("bugs", bugs);
-
-		return "dashboard.jsp";
-	}
 }
--- src/main/java/com/javacollab/memorydump/services/BugService.java
+++ src/main/java/com/javacollab/memorydump/services/BugService.java
@@ -1,5 +1,15 @@
 package com.javacollab.memorydump.services;
 
+import com.javacollab.memorydump.repositories.BugRepo;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class BugService {
 
+    private final BugRepo bugRepo;
+
+    public BugService(BugRepo bugRepo){
+        this.bugRepo = bugRepo;
+    }
 }
